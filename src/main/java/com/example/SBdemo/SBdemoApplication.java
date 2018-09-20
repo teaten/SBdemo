41f3e519fae971ec908f65d86fa4c11ed78ec0c4
teaten <teaten@qq.com>
2018-09-21 00:26:29 +0800

    不用@mapperscan

--- src/main/java/com/example/SBdemo/SBdemoApplication.java
+++ src/main/java/com/example/SBdemo/SBdemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.SBdemo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
-@MapperScan("com.example.SBdemo.dao")
 public class SBdemoApplication {
 
 	public static void main(String[] args) {
